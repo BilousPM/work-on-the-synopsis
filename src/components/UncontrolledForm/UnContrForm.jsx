916fc0031a52e2f0b7e8634be5a0098bc4519a33
BilousPM <bilouspm@gmail.com>
2024-07-11 07:46:39 -0700

    uncontrolled form done

--- src/components/UncontrolledForm/UnContrForm.jsx
+++ src/components/UncontrolledForm/UnContrForm.jsx
@@ -1,18 +1,51 @@
+import s from "./FormStyles.module.css";
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const form = e.target;
+  // console.log("form submited");
+  console.log(form);
+  console.log(form.elements);
+  console.log(form.elements.userName);
+  const { userName, email, password } = form.elements;
+  console.log({
+    password: password.value,
+    userName: userName.value,
+    email: email.value,
+  });
+  form.reset();
+};
+
 const UnContrForm = () => {
   return (
-    <div>
-      <form>
-        <label>
+    <div className={s.wrapper}>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <label className={s.label}>
           <span>Name:</span>
-          <input type="text" placeholder="Enter yor first name..."></input>
+          <input
+            name="userName"
+            type="text"
+            placeholder="Enter yor first name..."
+            className={s.input}
+          ></input>
         </label>
-        <label>
+        <label className={s.label}>
           <span>Email:</span>
-          <input type="text" placeholder="Enter yor Email"></input>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter yor Email"
+            className={s.input}
+          ></input>
         </label>
-        <label>
+        <label className={s.label}>
           <span>Password:</span>
-          <input type="text" placeholder="Enter yor Password"></input>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter yor Password"
+            className={s.input}
+          ></input>
         </label>
         <button type="submit">Register</button>
       </form>
