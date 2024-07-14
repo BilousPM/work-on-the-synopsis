8a569d7a99d19396028e0222a00368785392fbbb
BilousPM <bilouspm@gmail.com>
2024-07-14 14:02:05 -0600

    form validation

--- src/components/Formik/FormikForm.jsx
+++ src/components/Formik/FormikForm.jsx
@@ -1,7 +1,28 @@
-import { Field, Form, Formik } from "formik";
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import s from "./FormikForm.module.css";
+import * as Yup from "yup";
 
 const FormikForm = () => {
+  const registerSchema = Yup.object({
+    name: Yup.string()
+      .required("This field is required")
+      .min(3, "Name must be more than 3 charts!")
+      .max(15, " Name must be lesa than 20 charts!")
+      .matches(/^[a-zA-Z]+$/, "Must be only chars!"),
+    surname: Yup.string()
+      .required("This field is required")
+      .min(3, "Surname must be more than 3 charts!")
+      .max(15, " Surname must be lesa than 20 charts!"),
+    age: Yup.number()
+      .required("This field is required")
+      .min(14, "You must be more than 14 years!")
+      .max(100, "Woow so slow!"),
+    role: Yup.string().oneOf(["user", "admin"]),
+    email: Yup.string()
+      .required("This field is required")
+      .email("Email is not valid!"),
+  });
+
   const initialValues = {
     name: "",
     surname: "",
@@ -12,29 +33,41 @@ const FormikForm = () => {
   };
   const handleSubmit = (data, actions) => {
     console.log(data);
-    console.log(actions);
+    // console.log(actions);
     actions.resetForm();
   };
 
   return (
     <div className={s.formwrapper}>
-      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Formik
+        validationSchema={registerSchema}
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+      >
         <Form className={s.form}>
           <label className={s.label}>
             <span>Name</span>
             <Field name="name" className={s.input} />
+            <ErrorMessage name="name" component="span" className={s.error} />
           </label>
           <label className={s.label}>
             <span>Surname:</span>
             <Field name="surname" className={s.input} />
+            <ErrorMessage name="surname" component="span" className={s.error} />
           </label>
           <label className={s.label}>
             <span>Age:</span>
             <Field name="age" className={s.input} />
+            <ErrorMessage name="age" component="span" className={s.error} />
           </label>
           <label className={s.label}>
             <span>Email:</span>
             <Field name="email" className={s.input} type="email" />
+            <ErrorMessage
+              name="email"
+              component="span"
+              className={s.error}
+            />{" "}
           </label>
           <label className={s.label}>
             <span>Role</span>
@@ -42,10 +75,12 @@ const FormikForm = () => {
               <option value="user">User</option>
               <option value="admin">Admin</option>
             </Field>
+            <ErrorMessage name="role" component="span" className={s.error} />
           </label>
           <label className={s.label}>
             <span>About</span>
             <Field name="about" as="textarea" className={s.input}></Field>
+            <ErrorMessage name="about" component="span" className={s.error} />
           </label>
           <button type="submit">Create</button>
         </Form>
